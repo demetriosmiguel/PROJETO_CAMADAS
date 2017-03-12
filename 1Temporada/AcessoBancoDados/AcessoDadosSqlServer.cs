259e157fd5adeeffc7d0b3112128ed8525389ab2
Demetrios Miguel Silva <demetriosmiguel@gmail.com>
2017-03-12 19:31:48 -0300

    Implementação do Metodo Consultar registros  do banco de dados;

--- 1Temporada/AcessoBancoDados/AcessoDadosSqlServer.cs
+++ 1Temporada/AcessoBancoDados/AcessoDadosSqlServer.cs
@@ -24,11 +24,13 @@ namespace AcessoBancoDados
         //Parâmetros que vão para o Banco
         private SqlParameterCollection sqlParameterCollection = new SqlCommand().Parameters;
 
+        //Limpar Parâmetros;
         public void LimparParametros()
         {
             sqlParameterCollection.Clear();
         }
 
+        //Adicionando Parâmetros;
         public void AdicionarParametros(string nomeParametro, object valorParametro)
         {
             sqlParameterCollection.Add(new SqlParameter(nomeParametro, valorParametro));
@@ -69,7 +71,43 @@ namespace AcessoBancoDados
         }
 
         //Consultar registros  do banco de dados 
+        public DataTable ExecutarConsulta(CommandType commandType, String nomeStoredProcedureOuTextoSql)
+        {
+            try
+            {
+                // Criar conexao;
+                SqlConnection sqlConnection = CriarConexao();
+                //Abrir conexao;
+                sqlConnection.Open();
+                // Criar comando que leva as informações para o Banco;
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                //Colocando as informações dentro dos comandos  (Dentro da caixa que vai trafegar na conexao);
+                sqlCommand.CommandType = commandType;
+                sqlCommand.CommandText = nomeStoredProcedureOuTextoSql;
+                sqlCommand.CommandTimeout = 7200; // Em segundos
 
+                // Adicionar os parâmetros no comando;
+                foreach (SqlParameter sqlParameter in sqlParameterCollection)
+                {
+                    sqlCommand.Parameters.Add(new SqlParameter(sqlParameter.ParameterName, sqlParameter.Value));
+                }
+
+                // Criar um adapdador
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                // DataTable = Tabela de dados vazia onde vou colcoar os dados que vem do banco
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+
+                return dataTable;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
 
     }
 }
